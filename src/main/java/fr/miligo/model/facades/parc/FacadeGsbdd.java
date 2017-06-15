e3d3c9623925459392f9ff7709ceb72076b49dee
mika <mikarue@yahoo.fr>
2017-06-15 17:48:15 +0200

    Debut modif Gsbdd
    creation fonction de nouvelle instance de gsbdd

--- src/main/java/fr/miligo/model/facades/parc/FacadeGsbdd.java
+++ src/main/java/fr/miligo/model/facades/parc/FacadeGsbdd.java
@@ -1,8 +1,27 @@
 package fr.miligo.model.facades.parc;
 
+import javax.ejb.Stateless;
+
 import fr.miligo.common.AbstractFacade;
 import fr.miligo.model.entities.parc.Gsbdd;
 
+@Stateless
 public class FacadeGsbdd extends AbstractFacade<Gsbdd> {
 
+	
+	
+	/**
+	 * Méthode de fabrication d'un gsbdd 
+	 * @param trigramme
+	 * @param libelle
+	 * @return une nouvelle instance de grade
+	 */
+	public Gsbdd newInstance(String codeCredo, String libelle) {
+		Gsbdd g = super.newInstance();
+		g.setNumeroCredo(codeCredo);
+		g.setLibelle(libelle);
+		return g;
+	}	
+	
+	
 }
