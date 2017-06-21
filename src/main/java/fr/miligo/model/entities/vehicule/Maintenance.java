84a7f9e05b8fed329b51b3b23f9e3c25596a2a61
rbenvenuti <remi.benvenuti@gmail.com>
2017-06-21 17:03:10 +0200

    Sonar: suppression import inutilisé

--- src/main/java/fr/miligo/model/entities/vehicule/Maintenance.java
+++ src/main/java/fr/miligo/model/entities/vehicule/Maintenance.java
@@ -10,7 +10,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 /**
