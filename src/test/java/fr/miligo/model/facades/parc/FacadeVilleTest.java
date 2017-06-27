21ce2d2772e44c7e4b1aca824df4a17c75a9d36b
zyra83 <zyra83@hotmail.com>
2017-06-28 00:05:19 +0200

    Fixes #40 TU 100% sur classe FacadeVille + Jacoco
    Ajout de la partie jacoco pour créer les fichiers de rapport de couverture de code à envoyer à sonar lors du sonar:sonar.
    La classe FacadeVille a servi de cobaye.

--- src/test/java/fr/miligo/model/facades/parc/FacadeVilleTest.java
+++ src/test/java/fr/miligo/model/facades/parc/FacadeVilleTest.java
@@ -1,19 +1,7 @@
-/**
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright (c) 2016 C2B2 Consulting Limited and/or its affiliates.
- * All rights reserved.
- *
- * The contents of this file are subject to the terms of the Common Development
- * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License.  You can
- * obtain a copy of the License at
- * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
- * or packager/legal/LICENSE.txt.  See the License for the specific
- * language governing permissions and limitations under the License.
- *
- * When distributing the software, include this License Header Notice in each
- * file and include the License file at packager/legal/LICENSE.txt.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package fr.miligo.model.facades.parc;
 
@@ -39,14 +27,14 @@ import static org.junit.Assert.assertThat;
  * author: mertcaliskan
  */
 @RunWith(Arquillian.class)
-public class VilleDaoTest {
+public class FacadeVilleTest {
 
     @Inject
     private FacadeVille facadeVille;
 
     @Deployment
     public static WebArchive createDeployment() {
-        // Import Maven runtime dependencies
+        // Import Maven runtime dependencies 
         File[] files = Maven.resolver()
                 .loadPomFromFile("pom.xml")
                 .importRuntimeDependencies()
@@ -54,14 +42,14 @@ public class VilleDaoTest {
                 .withTransitivity()
                 .asFile();
 
-        // Create deploy file    
+        // Create deploy file     
         WebArchive war = ShrinkWrap.create(WebArchive.class, "arquillian-example.war")
                 .addClass(Ville.class)
                 .addClass(FacadeVille.class)
                 .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
                 .addAsLibraries(files);
 
-        // Show the deploy structure
+        // Show the deploy structure 
         System.out.println(war.toString(true));
 
         return war;
@@ -72,10 +60,30 @@ public class VilleDaoTest {
     @UsingDataSet("datasets/ville.yml")
     public void shouldReturnAllPerson() throws Exception {
         List<Ville> personList = facadeVille.readAll();
-
         assertNotNull(personList);
-        assertThat(personList.size(), is(1));
+        assertThat(personList.size(), is(2));
         assertThat(personList.get(0).getCodePostal(), is("83190"));
         assertThat(personList.get(0).getLibelle(), is("Ollioules"));
     }
+
+    /**
+     * Test of newInstance method, of class FacadeVille.
+     */
+    @Test
+    public void testNewInstance_String_String() throws Exception {
+        Ville ville = facadeVille.newInstance("83150", "Bandol");
+        assertNotNull(ville);
+        assertThat(ville.getCodePostal(), is("83150"));
+        assertThat(ville.getLibelle(), is("Bandol"));
+    }
+
+    @Test
+    @UsingDataSet("datasets/ville.yml")
+    public void testReadByNom() throws Exception {
+        Ville ville = facadeVille.readbyNom("Sanary-sur-Mer");
+        assertNotNull(ville);
+        assertThat(ville.getCodePostal(), is("83110"));
+        assertThat(ville.getLibelle(), is("Sanary-sur-Mer"));
+    }
+
 }
